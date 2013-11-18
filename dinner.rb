7ad849afb1d9153e9f9b6dfa8bb03776d0960b44
rlh6f <github.rharris@neverbox.com>
2013-11-18 05:21:03 -0800

    Menu done

--- dinner.rb
+++ dinner.rb
@@ -1,6 +1,11 @@
 class Dinner
   attr_accessor :guests
-  def initialize (*args)
+  attr_accessor :menu
+
+  Menu=Struct.new(:diet, :proteins, :veggies, :desserts)
+
+  def initialize (diet, proteins=["Tofurkey", "Hummus"], veggies=[:ginger_carrots, :potatoes, :yams], desserts={:pies => [:pumkin_pie], :other => ["Chocolate Moose"], :molds => [:cranberry, :mango, :cherry]})
+    @menu=Menu.new(diet, proteins, veggies, desserts)
 
   end
 
