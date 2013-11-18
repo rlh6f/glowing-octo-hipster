0da56376193c106926842294b1f4b5b8d6a13f21
rlh6f <github.rharris@neverbox.com>
2013-11-18 00:34:52 -0800

    EvenNumber rspec and class framework

---
+++ even_number.rb
@@ -0,0 +1,3 @@
+class EvenNumber
+
+end
---
+++ even_number_spec.rb
@@ -0,0 +1,21 @@
+require "#{File.dirname(__FILE__)}/even_number"
+
+describe EvenNumber do
+
+  it "should only allow even numbers" do
+      pending
+  end
+
+  it "should get the next even number" do
+      pending
+  end
+
+  it "should compare even numbers" do
+      pending
+  end
+
+  it 'should generate a range of even numbers' do
+      pending
+  end
+
+end
